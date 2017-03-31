58d1334182738c5157ace878385605363517ceda
mini5 <mini5@mini5s-Mac-mini.local>
2017-03-31 10:23:29 +0200

    prvi commit

---
+++ practice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  practice
+//
+//  Created by mini5 on 31.03.2017..
+//  Copyright © 2017. mini5. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ practice/homeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  homeViewController.swift
+//  practice
+//
+//  Created by mini5 on 31.03.2017..
+//  Copyright © 2017. mini5. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class homeViewController:UIViewController{
+    
+    
+    @IBOutlet weak var demo: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.demo.text = "demo2"
+    }
+    
+}                     
